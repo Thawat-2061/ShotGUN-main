2088a40ad1624753615d45789922b5d011dd1636
Thawat-2061 <65011212061@msu.ac.th>
2026-05-12 23:48:49 +0700

    fff

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ const ENDPOINTS = {
   UPLOAD: `${BASE_URL}/upload`,
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
-  image_url: "http://192.168.1.111/",
+  image_url: "https://displaying-epinions-diagnostic-merger.trycloudflare.com/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
