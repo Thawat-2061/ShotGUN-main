3cc5a9cda85e91d9e9688a388098a9f5f7e6327f
Thawat-2061 <65011212061@msu.ac.th>
2026-05-12 23:54:02 +0700

    sds

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ const ENDPOINTS = {
   UPLOAD: `${BASE_URL}/upload`,
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
-  image_url: "https://api-test-xi-one.vercel.app/",
+  image_url: "http://100.91.163.77:3000/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
