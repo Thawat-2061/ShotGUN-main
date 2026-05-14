5b1503e0c1b69ed32b97d9b190ab3e4f18060517
Thawat-2061 <65011212061@msu.ac.th>
2026-05-14 09:22:19 +0700

    tsrt

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@
 // const BASE_URL = "http://localhost:3000";
 
 // maxaaaaaa
-const BASE_URL = "https://opposite-film-movement-page.trycloudflare.com";
+const BASE_URL = "https://cheap-describes-olympics-villages.trycloudflare.com";
 
 
 const ENDPOINTS = {
@@ -22,7 +22,7 @@ const ENDPOINTS = {
   UPLOAD: `${BASE_URL}/upload`,
   GETPROJECTIMAGES: `${BASE_URL}/project/images`,
   DELETEPROJECT: `${BASE_URL}/deleteProject`,
-  image_url: "https://opposite-film-movement-page.trycloudflare.com/",
+  image_url: "https://cheap-describes-olympics-villages.trycloudflare.com/",
   videos: `${BASE_URL}/videos`,
   IMAGE_URL: `${BASE_URL}/`,
   UPLOAD_VIDEO: `${BASE_URL}/upload/video`,
